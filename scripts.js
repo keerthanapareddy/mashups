9b3d4bbf87f0e47c0ccab3ba4ec1da18d1ab344e
keerthanapareddy <skp440@nyu.edu>
2018-03-26 16:40:41 -0400

    mashing APIs

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 var app = {
   location:{},
+  latitude:{},
+  longitude:{},
 	social_score:{},
-	supported_market:{},
+
 
   initialize: function() {
      $('#searchlocation').submit(function(e){
       e.preventDefault();
       console.log("Yahaan!");
       app.initMap();
-      app.getGoogleMapAPI();
+      app.getGoogleMapAPI(location);
+
 
       e.preventDefault();
   });
@@ -17,9 +20,13 @@ var app = {
 	makeHTML: function() {
 		var theHTML = '';
 		// for (var i = 0; i < app.social_score.quarters.length; i++){
-			for (var j = 0; j < app.social_score[6].scores.length; j++){
-      theHTML += "<div class='spatialAI'>";
-			theHTML += "<h3>" + app.social_score[6].scores[j].category_name + "</h3>";
+			for (var j = 0; j < app.social_score[7].scores.length; j++){
+      theHTML += "<div id='apiresults'>";
+			theHTML += "<p>" + app.social_score[7].scores[j].category_name + "<p>";
+      theHTML += "<p>" + app.social_score[7].scores[j].value + "<p>";
+
+
+
       	// theHTML += "<h3>" + app.location.lat + "</h3>";
         // theHTML += "<h3>" + app.location.lng + "</h3>";
       // theHTML += "<h3>" + app.social_score.quarters[6].scores[j].category_name+ "</h3>";
@@ -28,20 +35,20 @@ var app = {
 			theHTML += "</div>";
 		}
   // }
-     $('main').html(theHTML);
+     $('#apiresults').html(theHTML);
   },
 
 
 
    getspatialAIData: function(location) {
 		console.log("Get spatial Data");
-    var latitude = $("#latitude").val();
-    console.log(latitude);
-    var longitude = $("#longitude").val();
-    console.log(longitude);
+    var spatialLatitude = app.latitude;
+    console.log(spatialLatitude);
+    var spatialLongitude = app.longitude;
+    console.log(spatialLongitude);
 		// var currentSearchWord = 'lat=39.106395&lng=-84.515844';
 		// var spatialURL = 'https://api.spatial.ai/neighborhood/point/social_score?' + currentSearchWord + '&apikey=';
-    var spatialURL = 'https://api.spatial.ai/neighborhood/point/social_score?' + 'lat=' + latitude + '&lng=' + longitude +'&apikey=';
+    var spatialURL = 'https://api.spatial.ai/neighborhood/point/social_score?' + 'lat=' + spatialLatitude + '&lng=' + spatialLongitude +'&apikey=';
 		var spatialKey = 'H8ylnFsByJB6iH3w8lnrJsmk2DPPGOqi';
 		var spatialReqURL = spatialURL + spatialKey;
 		console.log(spatialReqURL);
@@ -107,16 +114,16 @@ var app = {
     });
   },
 
-  getGoogleMapAPI : function(){
+  getGoogleMapAPI : function(location){
     console.log("Get Google location");
-      var address = $("#address").val();
+    var address = $("#address").val();
     var mapURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address +'&key=';
 		var mapKey = 'AIzaSyDPWS54PNjzC-65SUDM5GsWOmdPKjy2nis';
 		var mapReqURL = mapURL + mapKey;
 		console.log(mapReqURL);
 
-    // $(document).ready(function() {
-    //  console.log("document 2 ready");
+    $(document).ready(function() {
+     console.log("document 2 ready");
     //ajax call here
     $.ajax({
       url: mapReqURL,
@@ -133,16 +140,25 @@ var app = {
       success: function(data){
         console.log("success");
         console.log(data);
-        // app.social_score = data.social_score.quarters;
+        app.latitude = data.results[0].geometry.location.lat;
+        app.longitude = data.results[0].geometry.location.lng;
         // // for (var i = 0; i < app.social_score.length; i++){
         // // 	app. getspatialAIData(app.social_score[i]);
         // // }
-        // console.log(app.social_score);
-
+        console.log(app.latitude);
+        console.log(app.longitude);
+        console.log("getting spatial now");
+        app.getspatialAIData(location);
         // app.makeHTML();
       }
     });
-  // });
+  });
 
   }
 };
+
+function myFunction() {
+    var points = [40, 100, 1, 5, 25, 10];
+    points.sort(function(a, b){return b-a});
+    console.log(points[0]);
+}
