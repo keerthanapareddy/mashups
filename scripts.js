bf647b9b4d05180d874384e40e81390bbca6f844
keerthanapareddy <skp440@nyu.edu>
2018-03-25 22:32:19 -0400

    ajax call working

--- scripts.js
+++ scripts.js
@@ -4,27 +4,34 @@ var app = {
 	supported_market:{},
 
   initialize: function() {
-		app.getspatialAIData();
+     $('#fields').submit(function(e){
+      e.preventDefault();
+      console.log("Yahaan!");
+      app.getspatialAIData(location);
+      e.preventDefault();
+  });
 	},
 
-	makeHTML: function() {
+	makeHTML: function(thething) {
 		var theHTML = '';
 		// for (var i = 0; i < app.social_score.quarters.length; i++){
-				for (var j = 0; j < app.social_score.quarters[6].scores.length; j++){
+			// for (var j = 0; j < app.social_score.quarters.length; j++){
       theHTML += "<div class='spatialAI'>";
-			theHTML += "<h3>" + app.social_score.quarters[6].scores[j].category_name+ "</h3>";
+			// theHTML += "<h3>" + app.social_score.quarters[6] + "</h3>";
+      	theHTML += "<h3>" + app.location + "</h3>";
+      // theHTML += "<h3>" + app.social_score.quarters[6].scores[j].category_name+ "</h3>";
 			// theHTML += "<h3>" + app.location + "</h3>";
 
 			theHTML += "</div>";
-		}
+		// }
   // }
+     $('main').html(theHTML);
 
-		$('main').html(theHTML);
 	},
 
 
 
-   getspatialAIData: function() {
+   getspatialAIData: function(thething) {
 		console.log("Get spatial Data");
     var latitude = $("#latitude").val();
     console.log(latitude);
@@ -37,28 +44,30 @@ var app = {
 		var spatialReqURL = spatialURL + spatialKey;
 		console.log(spatialReqURL);
 
-		$.ajax({
-			url: spatialReqURL,
-		  type: 'GET',
-			dataType: 'json',
+    $(document).ready(function() {
+     console.log("document 2 ready");
+		//ajax call here
+    $.ajax({
+      url: spatialReqURL,
+      type: 'GET',
+      dataType: 'json',
+
+
+      check: console.log("ajax call"),
+      error: function(err){
+        console.log("Uh oh...");
+        console.log(err);
+      },
+
       success: function(data){
         console.log("success");
-				// debugger();
-				//console.log(data);
-				//app.social_score = data.social_score;
-				//console.log(app.social_score);
-				// console.log(data.respose.);
-				//app.makeHTML();
-			},
-			error: function(err){
-				console.log("Uh oh...");
-				console.log(err);
-			}
-
-		});
-    console.log(spatialReqURL);
+        console.log(data);
+        app.location = data.location;
+        console.log(app.location);
+        app.makeHTML(app.location);
+      }
+    });
+  });
+    // console.log(spatialReqURL);
 	}
-
-
-
 };
